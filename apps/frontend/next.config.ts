f06bd138904ae9419d7a42ddc873e343f7724ef5
haddadaziz <haddad.aziz27@gmail.com>
2026-07-21 17:45:28 +0100

    fix: vercel deployment

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
-  outputFileTracingRoot: path.resolve(__dirname, "../../"),
-  turbopack: {
-    root: path.resolve(__dirname, "../../"),
-  },
   images: {
     formats: ['image/avif', 'image/webp'],
   },
